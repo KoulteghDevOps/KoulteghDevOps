89464f33b96a924d7101bf64660d68ed15462af5
KoulteghDevOps <koultegh@outlook.com>
2023-04-19 08:14:28 +0100

    Catalogue file updated

--- Roboshop/catalogue.sh
+++ Roboshop/catalogue.sh
@@ -1,5 +1,7 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 useradd roboshop
 mkdir /app 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
@@ -7,11 +9,11 @@ cd /app
 unzip /tmp/catalogue.zip
 cd /app 
 npm install 
-cp catalogue.service /etc/systemd/system/catalogue.service
+# cp catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue 
 systemctl start catalogue
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+# cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
 mongo --host mongodb-dev.gilbraltar.co.uk </app/schema/catalogue.js
 systemctl restart catalogue
