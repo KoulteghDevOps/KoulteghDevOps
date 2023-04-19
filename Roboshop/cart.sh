2f044c5ef5586f54d4552dd73d2037848ba10ae3
KoulteghDevOps <koultegh@outlook.com>
2023-04-19 09:55:46 +0100

    Updated cart file

--- Roboshop/cart.sh
+++ Roboshop/cart.sh
@@ -1,12 +1,12 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-cp catalogue.service /etc/systemd/system/cart.service
+cp cart.service /etc/systemd/system/cart.service
 useradd roboshop
 mkdir /app 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip 
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip 
 cd /app 
 unzip /tmp/cart.zip
-cd /app 
+# cd /app 
 npm install 
 # cp catalogue.service /etc/systemd/system/cart.service
 systemctl daemon-reload
