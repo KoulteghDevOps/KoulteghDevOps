226a00800164d5cdfe1d95b4f14bd06a5646be5b
KoulteghDevOps <koultegh@outlook.com>
2023-04-20 21:20:42 +0100

    All Shell files Updated

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -2,7 +2,8 @@ echo -e "\e[36m>>>>>>>>> Install Nginx <<<<<<<<<<<\e[0m"
 dnf install nginx -y 
 
 echo -e "\e[36m>>>>>>>>> Copy App Config File <<<<<<<<<<<\e[0m"
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp /home/centos/KoulteghDevOps/roboshop.conf /etc/nginx/default.d/roboshop.conf
+# cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[36m>>>>>>>>> Empty the HTML Location <<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/* 
@@ -13,7 +14,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 echo -e "\e[36m>>>>>>>>> Change Directory to Nginx HTML <<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html 
 
-echo -e "\e[36m>>>>>>>>> Unxip App File <<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Unzip App File <<<<<<<<<<<\e[0m"
 unzip /tmp/frontend.zip
 
 echo -e "\e[36m>>>>>>>>> Start Nginx <<<<<<<<<<<\e[0m"
